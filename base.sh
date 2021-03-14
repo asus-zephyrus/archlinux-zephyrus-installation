1b5bf71adc3421464c9d9100de197c156b408caf
Archiman Chakraborty <archimanac@gmail.com>
2021-03-14 06:12:54 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -12,12 +12,12 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arctechsol.localdomain arctechsol" >> /etc/hosts
 echo root:password | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 ebtables iptables ipset firewalld flatpak sof-firmware acpid
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-user-dirs xdg-utils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync reflector tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 ebtables iptables flatpak sof-firmware
 
-pacman -S --noconfirm xf86-video-amdgpu
+pacman -S --noconfirm xf86-video-amdgpu xf86-video-intel
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
@@ -29,11 +29,9 @@ systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
 
 useradd -m arctech
-echo arctech:password | chpasswd
+echo arctech:sam.1234 | chpasswd
 usermod -aG libvirt arctech
 
 echo "arctech ALL=(ALL) ALL" >> /etc/sudoers.d/arctech
