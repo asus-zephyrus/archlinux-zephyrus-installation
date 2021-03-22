87d313ea25dd27758eea67006abb81744d2d6c0d
Archiman Chakraborty <archimanac@gmail.com>
2021-03-22 11:49:05 +0000

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-sudo systemctl set-ntp true
+#sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
 sudo virsh net-autostart default && sudo virsh net-start default
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/pikaur.git
+#cd pikaur/
+#makepkg -si --noconfirm
 
-git clone https://aur.archlinux.org/yay-git.git
-cd yay-git/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/yay-git.git
+#cd yay-git/
+#makepkg -si --noconfirm
 
-git clone https://aur.archlinux.org/paru.git
-cd paru/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/paru.git
+#cd paru/
+#makepkg -si --noconfirm
 
 sudo pacman -S xorg qtile dmenu nitrogen picom rofi lxappearance lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings alacritty flatpak wget ttf-dejavu ttf-liberation noto-fonts thunar materia-gtk-theme papirus-icon-theme google-chrome
 
-git clone https://aur.archlinux.org/all-repository-fonts.git
-cd all-repository-fonts/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/all-repository-fonts.git
+#cd all-repository-fonts/
+#makepkg -si --noconfirm
 
 sudo systemctl enable lightdm
 
