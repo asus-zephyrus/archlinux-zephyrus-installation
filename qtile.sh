7d90c37481415425c7764390521c099aea716f27
Archiman Chakraborty <archimanac@gmail.com>
2021-03-22 12:01:39 +0000

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -3,11 +3,13 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-#sudo virsh net-autostart default && sudo virsh net-start default
+sudo virsh net-autostart default && sudo virsh net-start default
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+sudo reflector --fastest 10 --latest 10 --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
+
+#git clone https://aur.archlinux.org/pikaur.git
+#cd pikaur/
+#makepkg -si --noconfirm
 
 #git clone https://aur.archlinux.org/yay-git.git
 #cd yay-git/
@@ -24,7 +26,6 @@ sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-
 #makepkg -si --noconfirm
 
 sudo systemctl enable lightdm
-
-/bin/echo -e "\e[1;32mDone! SYSTEM REBOOTS IN 5 SECONDS \e[0m"
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
 reboot
