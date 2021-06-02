0a706a2dcecf2a0fc66fac67f6851a5c93f092d2
Archiman Chakraborty <archimanac@gmail.com>
2021-06-02 12:35:49 +0000

    Add new file

---
+++ amdgpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Intel & AMD GPU Drivers, Vulkan Drivers, OpenCL, OpenGL, Direct3D and Other Dependencies
+
+
+paru -S xf86-video-intel xf86-video-amdgpu xf86-video-ati libclc ocl-icd opencl-headers opencl-mesa lib32-opencl-mesa opencl-amd libva-intel-driver vulkan-intel libmfx lib32-vulkan-intel amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-mesa-layers vulkan-radeon vulkan-tools vulkan-validation-layers python-glfw vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers
