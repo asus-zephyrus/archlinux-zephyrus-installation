9f9baa4aae3fab78a26b499d0e30b4e991f3dc22
Archiman Chakraborty <archimanac@gmail.com>
2021-09-07 16:48:05 +0000

    Update amdgpu.sh

---
+++ gpuDriver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Intel & AMD GPU Drivers, Vulkan Drivers, OpenCL, OpenGL, Direct3D and Other Dependencies
+
+
+paru -S xf86-video-intel xf86-video-amdgpu libclc ocl-icd opencl-headers opencl-mesa lib32-opencl-mesa opencl-amd libva-intel-driver vulkan-intel libmfx lib32-vulkan-intel amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-mesa-layers vulkan-radeon vulkan-tools vulkan-validation-layers python-glfw vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
