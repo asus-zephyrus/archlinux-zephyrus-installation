5cbd4dd5d007e156c72e747b9cf3081361b8a533
Archiman Chakraborty <archimanac@gmail.com>
2021-10-24 20:12:57 +0000

    Update gpuDriver.sh

---
+++ amdgpudriver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Intel & AMD GPU Drivers, Vulkan Drivers, OpenCL, OpenGL, Direct3D and Other Dependencies
+
+
+paru -S xf86-video-amdgpu amdgpu-pro-libgl lib32-amdgpu-pro-libgl lib32-vulkan-amdgpu-pro llvm-amdgpu mhwd-amdgpu opencl-amd vulkan-amdgpu-pro amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-radeon vulkan-tools vulkan-validation-layers vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers gstreamer-vaapi
