26c719426d15ebfb1c917877eff8366eebc86a48
Archiman Chakraborty <archimanac@gmail.com>
2021-10-27 20:07:10 +0000

    Update amdgpudriver.sh

--- amdgpudriver.sh
+++ amdgpudriver.sh
@@ -3,4 +3,4 @@
 #Intel & AMD GPU Drivers, Vulkan Drivers, OpenCL, OpenGL, Direct3D and Other Dependencies
 
 
-paru -S xf86-video-amdgpu amdgpu-pro-libgl lib32-amdgpu-pro-libgl lib32-vulkan-amdgpu-pro llvm-amdgpu mhwd-amdgpu opencl-amd vulkan-amdgpu-pro amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-radeon vulkan-tools vulkan-validation-layers vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers gstreamer-vaapi
+paru -S xf86-video-amdgpu amdgpu-pro-libgl lib32-amdgpu-pro-libgl lib32-vulkan-amdgpu-pro llvm-amdgpu mhwd-amdgpu opencl-amd vulkan-amdgpu-pro amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-radeon vulkan-tools vulkan-validation-layers vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers gstreamer-vaapi opencl-mesa lib32-opencl-mesa mhwd-amdgpu neard lib32-vulkan-intel lib32-libva-intel-driver libva-utils libmfx intel-opencl-clang intel-media-driver intel-graphics-compiler intel-gmmlib intel-compute-runtime vulkan-intel libva-intel-driver ipw2200-fw ipw2100-fw
