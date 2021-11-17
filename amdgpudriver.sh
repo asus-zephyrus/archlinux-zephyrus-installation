bd99a57d6cef861d9acdb7b714b65e5ba830d6bb
Archiman Chakraborty <archimanac@gmail.com>
2021-11-17 18:03:03 +0000

    Update amdgpudriver.sh

--- amdgpudriver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Intel & AMD GPU Drivers, Vulkan Drivers, OpenCL, OpenGL, Direct3D and Other Dependencies
-
-
-paru -S xf86-video-amdgpu amdgpu-pro-libgl lib32-amdgpu-pro-libgl lib32-vulkan-amdgpu-pro llvm-amdgpu mhwd-amdgpu opencl-amd vulkan-amdgpu-pro amdvlk spirv-tools vulkan-extra-layers vulkan-extra-tools vulkan-headers vulkan-icd-loader vulkan-radeon vulkan-tools vulkan-validation-layers vkd3d lib32-amdvlk lib32-vkd3d lib32-vulkan-icd-loader lib32-vulkan-radeon lib32-vulkan-validation-layers gstreamer-vaapi opencl-mesa lib32-opencl-mesa mhwd-amdgpu neard lib32-vulkan-intel lib32-libva-intel-driver libva-utils libmfx intel-opencl-clang intel-media-driver intel-graphics-compiler intel-gmmlib intel-compute-runtime vulkan-intel libva-intel-driver ipw2200-fw ipw2100-fw dxvk-mingw-git lib32-vulkan-radeon lib32-vulkan-mesa-layers lib32-vulkan-intel lib32-vulkan-icd-loader lib32-vkd3d vkd3d vulkan-tools vulkan-swrast vulkan-radeon vulkan-mesa-layers vulkan-intel vulkan-icd-loader spirv-tools ipw2100-fw ipw2200-fw intel-ucode libva-intel-driver tbb vulkan-intel whois xf86-video-intel intel-compute-runtime intel-gmmlib intel-graphics-compiler intel-media-driver intel-opencl-clang libmfx libva-utils lib32-libva-intel-driver lib32-vulkan-intel neard opencl-mesa xf86-video-amdgpu lib32-opencl-mesa mhwd-amdgpu
